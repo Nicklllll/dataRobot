2d626147daddf232dc56d1ece73230b0d01f4aab
AUTOBVT-QQ6B2KG\Administrator <547596559@qq.com>
2017-12-21 14:02:02 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  // {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  // { path: 'dashboard', component: DashboardComponent},
+  // { path: 'stock', component: StockManageComponent },
+  // {path: 'stock/:id', component: StockFormComponent},
 ];
 
 @NgModule({
